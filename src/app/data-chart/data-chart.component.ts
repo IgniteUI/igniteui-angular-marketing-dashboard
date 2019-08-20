a34e3620cb65bc4b820e33fe973bd625623db76b
HristoP96 <HPopov@infragistics.com>
2019-08-20 19:18:26 +0300

    fix data chart

--- src/app/data-chart/data-chart.component.ts
+++ src/app/data-chart/data-chart.component.ts
@@ -35,7 +35,7 @@ export class DataChartComponent implements OnInit {
   @ViewChild('emptyAreChartTooltipTemplate', {read: TemplateRef, static: false})
   public emptyreAChartTooltipTemplate: TemplateRef<any>;
 
-  @ViewChild('columnChartTooltipTemplate', {read: TemplateRef, static: true})
+  @ViewChild('columnChartTooltipTemplate', {read: TemplateRef, static: false})
   public columnChartTooltipTemplate: TemplateRef<any>;
 
 
@@ -68,8 +68,8 @@ export class DataChartComponent implements OnInit {
       if (this.chartInitialization) {
         this.initChart();
         this.setSeries(this.mediumMode);
-      } else {
-        if (this.mediumMode) {
+        this.chartInitialization = false;
+      } else if (this.mediumMode) {
           this.chartData = this.areaChartData;
         } else {
           this.chartData = data.end.trafficStats;
@@ -81,8 +81,6 @@ export class DataChartComponent implements OnInit {
             }
            });
         }
-      }
-      this.chartInitialization = false;
     });
   }
 
@@ -93,12 +91,13 @@ export class DataChartComponent implements OnInit {
       this.chartData = this.columnChartData;
       for (const series of this.columnSeries) {
         this.chart.series.add(series);
-        this.addToolTipLayer();
       }
+      this.addToolTipLayer();
     } else if (this.mediumMode === true && this.chartData !== this.areaChartData) {
       this.chart.series.clear();
       this.chartData = this.areaChartData;
       for (const series of this.areaSeries) {
+        series.xAxis = this.time;
         this.chart.series.add(series);
       }
       this.addToolTipLayer();
@@ -129,8 +128,7 @@ export class DataChartComponent implements OnInit {
     if (dataSource) {
       series.dataSource = this.prevSeriesDataSource;
       } else {
-        series.dataSource = this.columnChartData;
-
+      series.dataSource = this.columnChartData;
       }
     return series;
   }
@@ -141,7 +139,6 @@ export class DataChartComponent implements OnInit {
     const series = new IgxAreaSeriesComponent();
     series.name = name;
     series.valueMemberPath = name;
-    series.xAxis = this.time;
     series.yAxis = this.yAxis;
     series.brush = color;
     series.title = (name as string).toUpperCase();
@@ -168,10 +165,11 @@ export class DataChartComponent implements OnInit {
   public initChart() {
     this.columnSeries.push(this.setColumnSeriesData('sessions', this.time, 'session', 'Session', '#ffff33', '#ffff33'));
     // tslint:disable-next-line: max-line-length
+    this.columnSeries.push(this.setColumnSeriesData('sessionsPrev', this.time, 'session', 'Prev.Session', '#655F00', '#655F00', true));
+
     this.columnSeries.push(this.setColumnSeriesData('conversion', this.timeConvers, 'conversion', 'Conversions', '#66cc00', '#66cc00'));
 
     // tslint:disable-next-line: max-line-length
-    this.columnSeries.push(this.setColumnSeriesData('sessionsPrev', this.time, 'session', 'Prev.Session', '#655F00', '#655F00', true));
     // tslint:disable-next-line: max-line-length
     this.columnSeries.push(this.setColumnSeriesData('conversionPrev', this.timeConvers, 'conversion', 'Prev.Conversions', '#295001', '#295001', true));
 
@@ -181,5 +179,9 @@ export class DataChartComponent implements OnInit {
     this.areaSeries.push(this.setAreaSeriesData('referral', '#E1EEB5'));
     this.areaSeries.push(this.setAreaSeriesData('email', '#FFFFFF'));
 
+  }
+
+  check(event){
+    debugger;
   }
 }
